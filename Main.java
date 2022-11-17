2f9ef5b36ca7b84b1316279aa03b1fcd989ef7f8
iz.aia.h <imukisa024@gmail.com>
2022-11-17 16:37:45 +0300

    Sales class has been added

--- Main.java
+++ Main.java
@@ -1,26 +1,24 @@
 package CakeShop;
 
 import java.sql.SQLException;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("I know you love cakes");
 
         // Login password = new Login();
         // Scanner scanner = new Scanner(System.in);
         // String userName = "Izaiah";
-        // String password = "1234";
-
+        //// String password = "1234";
+        //
         // System.out.print("Enter User Name: ");
         // String name=scanner.next();
-
+        //
         // System.out.print("Enter Password: ");
         // String pass = scanner.next();
-
+        //
         // if(userName.equals(name)) {
         // if(password.equals(pass)) {
         // System.out.println("Welcome!!");
@@ -30,72 +28,35 @@ public class Main {
         // }else {
         // System.out.println("Please Check user Name");
         // }
+        // scanner.close();
+
+        DatabaseConnect.LoginAdmin();
+        AddCustomerInfo();
+    }
+
+    public static void AddCustomerInfo() throws SQLException {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to our Cake Shop");
+
+        System.out.println("Enter your name:");
+        String name = input.nextLine();
+
+        System.out.println("Enter your email:");
+        String email = input.nextLine();
+
+        System.out.println("Enter your contact:");
+        int contact = input.nextInt();
+        input.nextLine();
+
+        System.out.println("Enter your address:");
+        String address = input.nextLine();
+
+        Customer cs = new Customer(name, email, contact, address);
+        DatabaseConnect.AddCustomer(cs);
+
+        input.close();
 
     }
 
-=======
-	
-=======
-	static final Scanner scanner = new Scanner(System.in);
-	//create one single static scanner object to be used throughout the whole program
-	//this is done to prevent the input streams from closing and bringing errors.
-	//the scanner object is then passed as an argument to the method calls where it is going to be used
-
->>>>>>> d67dc143f802ca921f56ebecb6dc2e1e944fd103
-	public static void main(String[] args) throws SQLException {
-        System.out.println("I know you love cakes");
-        System.out.println("Admin login");
-        
-        //this is the method used to log admins into the system by validating the user name and password input 
-        //it also takes in a scanner object as a parameter so that we can read input from the user in the console
-        boolean loginValid = DatabaseConnect.LoginAdmin(scanner);
-        
-        if(loginValid == true) {
-            AddCustomerInfo();
-        }else {
-        	System.exit(0);
-        }
-        
-        
-    }
-	
-	public static void AddCustomerInfo() throws SQLException {
-		
-		System.out.println("Welcome to our Cake Shop");
-		
-		System.out.println("Enter your name:");
-		String customername = scanner.nextLine();
-		
-		System.out.println("Enter your email:");
-		String customeremail = scanner.nextLine();
-		
-		System.out.println("Enter your contact:");
-		int customercontact = scanner.nextInt();
-		scanner.nextLine();
-		
-		System.out.println("Enter your address:");
-		String customeraddress = scanner.nextLine();
-		
-		Customer cs = new Customer(customername,customeremail,customercontact,customeraddress);
-		DatabaseConnect.AddCustomer(cs);
-		
-		scanner.close();
-		
-	}
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
->>>>>>> 191405e13453e7a86bfd4b264c3c0ed4022eaf76
 }
