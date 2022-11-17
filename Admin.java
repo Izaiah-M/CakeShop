55bb73eefed0dc6ef8a49ce47d8f9959db8dfc47
SoulSolomon21 <sekamattesoul@gmail.com>
2022-11-17 11:21:36 +0300

    Connection to sqlite database established

--- Admin.java
+++ Admin.java
@@ -1,29 +1,29 @@
 package CakeShop;
 
 public class Admin {
-	private String name;
-	private String email;
-	private String contact;
-	private String address;
+	private String username;
+	private String password;
 	
 	public Admin() {
 		
 	}
-	
-	public String getName() {
-		return name;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	
-	public String getEmail() {
-		return email;
-	}
-	
-	public String getContact() {
-		return contact;
-	}
-	
-	public String getAddress() {
-		return address;
-	}
+
 	
 }
--- Customer.java
+++ Customer.java
@@ -3,13 +3,32 @@ package CakeShop;
 public class Customer {
 	private String name;
 	private String email;
-	private String contact;
+	private int contact;
 	private String address;
 	
-	public Customer() {
-		
+	public Customer(String name, String email, int contact, String address) {
+		this.setName(name);
+		this.setEmail(email);
+		this.setContact(contact);
+		this.setAddress(address);
 	}
-	
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setContact(int contact) {
+		this.contact = contact;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 	public String getName() {
 		return name;
 	}
@@ -18,7 +37,7 @@ public class Customer {
 		return email;
 	}
 	
-	public String getContact() {
+	public int getContact() {
 		return contact;
 	}
 	
